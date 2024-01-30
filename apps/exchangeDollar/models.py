f8d5a5b92f1fc415b1fad7244d289a23129a9410
Andy Huacre <andy.huacre@egas.com.mx>
2024-01-29 21:40:36 -0600

    crear funcion para consumir api y traer la info, variables entorno y creacion de modelo

--- apps/exchangeDollar/models.py
+++ apps/exchangeDollar/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
+class ExchangeRate (models.Model):
+    date: models.DateField(null=True)
+    usd_to_mxn: models.DecimalField(max_digits=10, default=0)
+    mxn_to_usd: models.DecimalField(max_digits=10, default=0)
+
+    def __str__(self):
+        return f"${self.usd_to_mxn}mxn - ${self.mxn_to_usd}"
+
 # Create your models here.
--- apps/exchangeDollar/views.py
+++ apps/exchangeDollar/views.py
@@ -1,3 +1,31 @@
-from django.shortcuts import render
+import requests 
+from django.conf import settings
+from django.http import JsonResponse
+from datetime import datetime, timedelta
 
-# Create your views here.
+# def mxn2usd(request):
+    # importar el serializer para manipular la bd
+    
+# def usd2mxn(request):
+    # importar el serializer para manipular la bd
+    
+def consumirApi(request):
+    url = settings.URL_API
+    fecha = datetime.now().date()
+    fecha_ten_days_ago = fecha - timedelta(days=10)
+
+    params = {'token':settings.TOKEN_API}
+
+    try:
+        response = requests.get(f'{url}/datos/{fecha_ten_days_ago}/{fecha}',params=params)
+
+        if(response.status_code == 200):
+            data = response.json()
+            # importar data en BD
+            return JsonResponse(data)
+        else:
+            return JsonResponse({'error': f'Error en la solicitud. Codigo {response.status_code}'}, status=500)
+    except requests.exceptions.RequestException as error:
+        return JsonResponse({'error': f'Error en la solicitud: {str(error)}'}, status=500)
+    
+
